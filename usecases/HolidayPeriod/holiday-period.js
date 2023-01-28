b96c2f47bbec6be35f0ebe4d6e5c9470afa930ae
JosselinTD <josselin.td@gmail.com>
2023-01-28 19:22:42 +0100

    feat(usecases/holiday-period): add cutByMonths method and associated tests

--- usecases/HolidayPeriod/holiday-period.js
+++ usecases/HolidayPeriod/holiday-period.js
@@ -1,10 +1,51 @@
 const Period = require("../../entities/Period/period");
 
 module.exports = class HolidayPeriod extends Period {
-  intercectWith(period) {
+  intersectWith(period) {
     return (
       this.getStartDate() <= period.getEndDate() &&
       this.getEndDate() >= period.getStartDate()
     );
+  }
+
+  cutByMonths() {
+    const periodsCuttedByMonth = [];
+    let cuttedPeriod = HolidayPeriod.#getOneDayPeriod(this.getStartDate());
+
+    do {
+      cuttedPeriod.#extendToLowerBetweenNextMonthAnd(this.getEndDate());
+
+      periodsCuttedByMonth.push(cuttedPeriod);
+
+      cuttedPeriod = HolidayPeriod.#getOneDayPeriod(cuttedPeriod.getEndDate());
+    } while (!this.#hasSameEndDate(cuttedPeriod));
+
+    return periodsCuttedByMonth;
+  }
+
+  #extendToLowerBetweenNextMonthAnd(date) {
+    this.#extendToNextMonth();
+    this.#shrinkIfTooLargeTo(date);
+  }
+
+  #extendToNextMonth() {
+    this.getEndDate().setUTCMonth(this.getEndDate().getUTCMonth() + 1);
+    this.getEndDate().setUTCDate(1);
+  }
+
+  #shrinkIfTooLargeTo(date) {
+    if (this.getEndDate() > date) {
+      this.setEndDate(new Date(date));
+    }
+  }
+
+  #hasSameEndDate(period) {
+    return (
+      this.getEndDate().toISOString() === period.getEndDate().toISOString()
+    );
+  }
+
+  static #getOneDayPeriod(date) {
+    return new HolidayPeriod(new Date(date), new Date(date));
   }
 };
--- usecases/HolidayPeriod/holiday-period.test.js
+++ usecases/HolidayPeriod/holiday-period.test.js
@@ -13,7 +13,7 @@ describe("holiday period", () => {
 
     test("should check if holiday is fully included in another period, answering true", () => {
       expect(
-        holiday.intercectWith(
+        holiday.intersectWith(
           new Period(new Date("2022-01-01"), new Date("2022-01-31"))
         )
       ).toBe(true);
@@ -21,7 +21,7 @@ describe("holiday period", () => {
 
     test("should check if holiday is fully included in another period, answering false", () => {
       expect(
-        holiday.intercectWith(
+        holiday.intersectWith(
           new Period(new Date("2022-05-01"), new Date("2022-05-31"))
         )
       ).toBe(false);
@@ -29,10 +29,85 @@ describe("holiday period", () => {
 
     test("should check if holiday is partially included in another period, answering true", () => {
       expect(
-        holiday.intercectWith(
+        holiday.intersectWith(
           new Period(new Date("2022-01-07"), new Date("2022-01-31"))
         )
       ).toBe(true);
+    });
+  });
+
+  describe("cutting", () => {
+    test("should return just one period with same start and end date when a cutting is not needed", () => {
+      const holiday = new HolidayPeriod(
+        new Date("2022-01-01"),
+        new Date("2022-01-15")
+      );
+      const cuttedPeriods = holiday.cutByMonths();
+      expect(cuttedPeriods.length).toBe(1);
+      expect(cuttedPeriods[0].getStartDate().toISOString()).toBe(
+        holiday.getStartDate().toISOString()
+      );
+      expect(cuttedPeriods[0].getEndDate().toISOString()).toBe(
+        holiday.getEndDate().toISOString()
+      );
+    });
+
+    test("should return multiple period on the same year", () => {
+      const holiday = new HolidayPeriod(
+        new Date("2022-01-15"),
+        new Date("2022-03-22")
+      );
+      const cuttedPeriods = holiday.cutByMonths();
+      expect(cuttedPeriods.length).toBe(3);
+      expect(cuttedPeriods[0].getStartDate().toISOString()).toBe(
+        new Date("2022-01-15").toISOString()
+      );
+      expect(cuttedPeriods[0].getEndDate().toISOString()).toBe(
+        new Date("2022-02-01").toISOString()
+      );
+      expect(cuttedPeriods[1].getStartDate().toISOString()).toBe(
+        new Date("2022-02-01").toISOString()
+      );
+      expect(cuttedPeriods[1].getEndDate().toISOString()).toBe(
+        new Date("2022-03-01").toISOString()
+      );
+      expect(cuttedPeriods[2].getStartDate().toISOString()).toBe(
+        new Date("2022-03-01").toISOString()
+      );
+      expect(cuttedPeriods[2].getEndDate().toISOString()).toBe(
+        new Date("2022-03-22").toISOString()
+      );
+    });
+
+    test("should return multiple period on multiple years", () => {
+      const holiday = new HolidayPeriod(
+        new Date("2022-01-15"),
+        new Date("2024-03-22")
+      );
+      const cuttedPeriods = holiday.cutByMonths();
+      expect(cuttedPeriods.length).toBe(27);
+      expect(cuttedPeriods[0].getStartDate().toISOString()).toBe(
+        new Date("2022-01-15").toISOString()
+      );
+      expect(cuttedPeriods[0].getEndDate().toISOString()).toBe(
+        new Date("2022-02-01").toISOString()
+      );
+      const marchingDate = new Date("2022-02-01");
+      for (let i = 1; i < 26; i++) {
+        expect(cuttedPeriods[i].getStartDate().toISOString()).toBe(
+          marchingDate.toISOString()
+        );
+        marchingDate.setUTCMonth(marchingDate.getUTCMonth() + 1);
+        expect(cuttedPeriods[i].getEndDate().toISOString()).toBe(
+          marchingDate.toISOString()
+        );
+      }
+      expect(cuttedPeriods[26].getStartDate().toISOString()).toBe(
+        new Date("2024-03-01").toISOString()
+      );
+      expect(cuttedPeriods[26].getEndDate().toISOString()).toBe(
+        new Date("2024-03-22").toISOString()
+      );
     });
   });
 });
